dc69ffa2d2daf8e0ceb25db477f6707b5d2e939c
Ubuntu <azureuser@gitsuture-vm.cvwlmssant0uxcx5dml2ma41ya.rx.internal.cloudapp.net>
2026-09-13 07:10:57 +0000

    chore: finalize Azure VM deployment config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,12 @@ module.exports = {
     {
       name: 'gitsuture',
       script: 'dist/index.js',
+      cwd: '/home/azureuser/gitsuture-ai',
       env: {
         NODE_ENV: 'production',
       },
+      out_file: '/var/log/gitsuture/out.log',
+      error_file: '/var/log/gitsuture/error.log',
       merge_logs: true,
       time: true,
     }
